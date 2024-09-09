455f8fd2639f5103a8cd4bcb98e9a2d290c31ea2
Gustavo Teles <ustavoteles@gmail.com>
2024-09-09 15:16:49 -0300

    add search features

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 function pesquisar() {
   let section = document.getElementById('resultados-pesquisa')
 
-  let resultados = ''
+  let campoPesquisa = document.getElementById('campo-pesquisa').value
 
+  if (!campoPesquisa) {
+    section.innerHTML = `
+    <div class="item-resultado">
+    <h1>NENHUM ATLETA FOI ENCONTRADO</h1>
+    </div>
+`
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  let resultados = ''
+  let marca = ''
+  let descricao = ''
+  let tags = ''
   //esse modo nao é uma boa pratica
 
   //para cada dado dentro da lista de dados
   for (let dado of dados) {
-    //mais igual vai somar +=
-    resultados += `
-
+    marca = dado.marca.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    //se titulo includes campoPesquisa
+    //entao, faça....
+    if (
+      marca.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      resultados += `
     <div class="item-resultado">
             <h2>
                 <a href="#" target="_blank">${dado.marca}</a>
@@ -20,10 +43,20 @@ function pesquisar() {
             <a href="${dado.link}" target="_blank"
                 >Mais informações</a
             >
+            </div>
+            `
+    }
+
+    //mais igual vai somar +=
+  }
+  if (!resultados) {
+    resultados = `
+    <div class="item-resultado">
+    <h1>NADA FOI ENCONTRADO</h1>
     </div>
 `
   }
-
+  //atribui os resultados gerados à seção HTML
   section.innerHTML = resultados
 }
 
